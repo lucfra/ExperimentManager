b9540dfbf3866c10e9f260473cd8986042295ba2
luca <igor_mio@hotmail.it>
2017-10-25 16:34:46 +0200

    replace_names

--- experiment_manager/utils.py
+++ experiment_manager/utils.py
@@ -59,7 +59,7 @@ def name_from_vars(var_dict, *vars_):
         for k, vv in var_dict.items():
             if v == vv:
                 new_k_v[k] = str(v)
-    return '_'.join(flatten_list(list(sorted(new_k_v.items()))))
+    return '_'.join(flatten_list(list(sorted(new_k_v.items())))).replace('[', '__').replace(']', '__').replace(',', '_')
 
 
 def GPU_CONFIG():
