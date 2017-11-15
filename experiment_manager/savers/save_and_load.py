c5f31942e9bbd2c015901aba2208ec0a0595cabc
luca_dell_laptop <igor_mio@hotmail.it>
2017-11-15 12:53:56 +0100

    bah... try fix newly found issue....

--- experiment_manager/savers/save_and_load.py
+++ experiment_manager/savers/save_and_load.py
@@ -506,20 +506,9 @@ class Saver:
             else:
                 return Saver.SKIP
 
-        def _unnest(_pt, res_):
-            if isinstance(res_[0], str):
-                save_dict[_pt[0] + '::' + res_[0]] = res_[1]
-            elif isinstance(res_[0], (list, tuple)):
-                for _r in res_: _unnest(_pt, _r)
-            else: save_dict[pt[0]] = _res
-
         save_dict = OrderedDict()
         for pt in processed_items:
-            _res = _compute_value(pt, save_dict)
-            if isinstance(_res, (list, tuple)):
-                _unnest(pt, _res)
-            else:
-                save_dict[pt[0]] = _compute_value(pt, save_dict)
+            save_dict[pt[0]] = _compute_value(pt, save_dict)
 
         if self.timer: save_dict['SKIP::Elapsed time (%s)' % self.timer.unit] = self.timer.elapsed_time()
 
