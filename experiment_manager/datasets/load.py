37009ef35b1904d0cf179e7ff59377af1c52af3c
luca <igor_mio@hotmail.it>
2017-12-18 13:19:50 +0100

    added code to load OMNIGLOT

--- experiment_manager/datasets/load.py
+++ experiment_manager/datasets/load.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from os.path import join
 
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
@@ -62,6 +62,8 @@ MINI_IMAGENET_FOLDER_RES84 = join(DATA_FOLDER, join('imagenet', 'mini_res84'))
 MINI_IMAGENET_FOLDER_V2 = join(DATA_FOLDER, join('imagenet', 'mini_v2'))
 MINI_IMAGENET_FOLDER_V3 = join(DATA_FOLDER, join('imagenet', 'mini_v3'))
 
+OMNIGLOT_RESIZED = join(DATA_FOLDER, 'omniglot_resized')
+
 
 def balanced_choice_wr(a, num, rand=None):
     rand = em.utils.get_rand_state(rand)
@@ -84,6 +86,93 @@ def mnist(folder=None, one_hot=True, partitions=None, filters=None, maps=None, s
     return em.Datasets.from_list(res)
 
 
+def meta_omniglot(folder=OMNIGLOT_RESIZED, std_num_classes=None, std_num_examples=None,
+                  one_hot_enc=True, rand=0, n_splits=None):
+    class OmniglotMetaDataset(em.MetaDataset):
+
+        def __init__(self, info=None, rotations=None, name='Omniglot', num_classes=None, num_examples=None):
+            super().__init__(info, name=name, num_classes=num_classes, num_examples=num_examples)
+            self._loaded_images = defaultdict(lambda: {})
+            self._rotations = rotations or [0, 90, 180, 270]
+            self.load_all()
+
+        def generate_datasets(self, rand=None, num_classes=None, num_examples=None):
+            rand = em.get_rand_state(rand)
+
+            if not num_examples: num_examples = self.kwargs['num_examples']
+            if not num_classes: num_classes = self.kwargs['num_classes']
+
+            clss = self._loaded_images if self._loaded_images else self.info['classes']
+
+            random_classes = rand.choice(list(clss.keys()), size=(num_classes,), replace=False)
+            rand_class_dict = {rnd: k for k, rnd in enumerate(random_classes)}
+
+            _dts = []
+            for ns in em.as_tuple_or_list(num_examples):
+                classes = balanced_choice_wr(random_classes, ns, rand)
+
+                all_images = {cls: list(clss[cls]) for cls in classes}
+                data, targets, sample_info = [], [], []
+                for c in classes:
+                    rand.shuffle(all_images[c])
+                    img_name = all_images[c][0]
+                    all_images[c].remove(img_name)
+                    sample_info.append({'name': img_name, 'label': c})
+                    data.append(clss[c][img_name])
+                    targets.append(rand_class_dict[c])
+
+                if self.info['one_hot_enc']:
+                    targets = em.to_one_hot_enc(targets, dimension=num_classes)
+
+                _dts.append(em.Dataset(data=np.array(np.stack(data)), target=targets, sample_info=sample_info,
+                                       info={'all_classes': random_classes}))
+            return em.Datasets.from_list(_dts)
+
+        def load_all(self):
+            from scipy.ndimage import imread
+            from scipy.ndimage.interpolation import rotate
+            from scipy.misc import imresize
+            _cls = self.info['classes']
+            _base_folder = self.info['base_folder']
+
+            for c in _cls:
+                all_images = list(_cls[c])
+                for img_name in all_images:
+                    img = imread(join(_base_folder, join(c, img_name)))
+
+                    img = 1. - imresize(img, size=(28, 28, 1), interp='lanczos') / 255.
+                    for rot in self._rotations:
+                        img = rotate(img,  rot, reshape=False)
+                        self._loaded_images[c + os.path.sep + 'rot_' + str(rot)][img_name] = img
+
+    # if sub_folders is None: sub_folders = ['train', 'val', 'test']
+    alphabets = os.listdir(folder)
+
+    labels_and_images = OrderedDict()
+    for alphabet in alphabets:
+        base_folder = join(folder, alphabet)
+        label_names = os.listdir(base_folder)  # all characters in one alphabet
+        labels_and_images.update({alphabet + os.path.sep + ln: os.listdir(join(base_folder, ln))
+                                  # all examples of each character
+                                  for ln in label_names})
+
+    # divide between training validation and test meta-datasets
+    rand = em.get_rand_state(rand)
+    all_clss = list(labels_and_images.keys())
+    rand.shuffle(all_clss)
+    n_splits = n_splits or (0, 1200, 1300, len(all_clss))
+
+    meta_dts = []
+    for start, end in zip(n_splits, n_splits[1:]):
+        meta_dts.append(OmniglotMetaDataset(info={
+            'base_folder': folder,
+            'classes': {k: labels_and_images[k] for k in all_clss[start: end]},
+            'one_hot_enc': one_hot_enc
+        }, num_classes=std_num_classes, num_examples=std_num_examples))
+
+    return em.Datasets.from_list(meta_dts)
+
+
 def meta_mini_imagenet(folder=MINI_IMAGENET_FOLDER_V3, sub_folders=None, std_num_classes=None,
                        std_num_examples=None, resize=84, one_hot_enc=True, load_all_images=True, h5=True):
     """
@@ -101,6 +190,7 @@ def meta_mini_imagenet(folder=MINI_IMAGENET_FOLDER_V3, sub_folders=None, std_num
     :param h5:  True (default) to use HDF5 files, when False search for JPEG images.
     :return:
     """
+
     class ImageNetMetaDataset(em.MetaDataset):
 
         def __init__(self, info=None, name='MiniImagenet', num_classes=None, num_examples=None):
@@ -114,7 +204,7 @@ def meta_mini_imagenet(folder=MINI_IMAGENET_FOLDER_V3, sub_folders=None, std_num
                 h5m = h5py.File(_file, 'r')
                 img_per_class = 600
                 for j in range(len(h5m['X'])):
-                    self._loaded_images[j//img_per_class][j] = np.array(h5m['X'][j], dtype=np.float32)/255.
+                    self._loaded_images[j // img_per_class][j] = np.array(h5m['X'][j], dtype=np.float32) / 255.
                     # images were stored as int
             else:
                 from scipy.ndimage import imread
@@ -194,7 +284,7 @@ def meta_mini_imagenet(folder=MINI_IMAGENET_FOLDER_V3, sub_folders=None, std_num
             data, targets = [], []
             for k, c in enumerate(sorted(self._loaded_images)):
                 data += list(self._loaded_images[c].values())
-                targets += [k]*600
+                targets += [k] * 600
             if self.info['one_hot_enc']:
                 targets = em.to_one_hot_enc(targets, dimension=len(self._loaded_images))
             _dts = [em.Dataset(data=np.stack(data), target=np.array(targets), name='MiniImagenet_full')]
@@ -224,9 +314,9 @@ def meta_mini_imagenet(folder=MINI_IMAGENET_FOLDER_V3, sub_folders=None, std_num
                 'file': file,
                 'one_hot_enc': one_hot_enc
             }, num_classes=std_num_classes, num_examples=std_num_examples))
-        # change name with shots...
-        # if std_num_classes and std_num_examples:
-        #     meta_dts[-1].name += '_shots_%s' % (std_num_examples[0]//std_num_classes)
+            # change name with shots...
+            # if std_num_classes and std_num_examples:
+            #     meta_dts[-1].name += '_shots_%s' % (std_num_examples[0]//std_num_classes)
     dts = em.Datasets.from_list(meta_dts)
     if load_all_images:
         import time
@@ -238,14 +328,17 @@ def meta_mini_imagenet(folder=MINI_IMAGENET_FOLDER_V3, sub_folders=None, std_num
 
 
 if __name__ == '__main__':
-    mmi = meta_mini_imagenet()
-    # ts = mmi.train.generate_datasets(num_classes=10, num_examples=(123, 39))
-    d1 = mmi.train.all_data(seed=0)
-    print(d1.train.dim_data, d1.train.dim_target)
+    # mmi = meta_mini_imagenet()
+    # # ts = mmi.train.generate_datasets(num_classes=10, num_examples=(123, 39))
+    # d1 = mmi.train.all_data(seed=0)
+    # print(d1.train.dim_data, d1.train.dim_target)
+    #
+    # mmiii = meta_mini_imagenet()
+    # # ts = mmi.train.generate_datasets(num_classes=10, num_examples=(123, 39))
+    # d2 = mmiii.train.all_data(seed=0)
+    # print(d2.train.dim_data, d2.train.dim_target)
+    #
+    # print(np.equal(d1.train.data[0], d2.train.data[0]))
 
-    mmiii = meta_mini_imagenet()
-    # ts = mmi.train.generate_datasets(num_classes=10, num_examples=(123, 39))
-    d2 = mmiii.train.all_data(seed=0)
-    print(d2.train.dim_data, d2.train.dim_target)
-
-    print(np.equal(d1.train.data[0], d2.train.data[0]))
+    res = meta_omniglot()
+    print(res)
