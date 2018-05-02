6569b12977986862cd3585c6140c1f3ddd790749
luca <igor_mio@hotmail.it>
2018-05-02 18:10:58 +0200

    deleted types and considered object

--- experiment_manager/search_strategies.py
+++ experiment_manager/search_strategies.py
@@ -32,25 +32,19 @@ def random(names, *args, max_time=500000):
         yield name_from_dict(dict_), dict_
 
 
-def grid(names, *rngs, types=None, _id=0, total=1, _verbose=True):
-    _grid = np.array(np.meshgrid(*rngs)).T.reshape((-1, len(rngs)))
+def grid(names, *rngs, _id=0, total=1, _verbose=True):
+    _grid = np.array(np.meshgrid(*rngs), dtype=object).T.reshape((-1, len(rngs)))
     d = len(_grid)
     slice_of_the_grid = _grid[int(_id/total*d): int((_id+1)/total*d)]
-    if isinstance(names[0], tuple):
-        print(names)
-        types = [n[1] for n in names]
-        names = [n[0] for n in names]
 
-    if not types:
-        types = [lambda _x: _x]*len(names)
     if _verbose:
         print('Process ', _id, ' will search in:')
         print(slice_of_the_grid)
     for e in slice_of_the_grid:
-        dict_ = {n: tp(_e) for n, _e, tp in zip(names, e, types)}
+        dict_ = {n: _e for n, _e in zip(names, e)}
         yield name_from_dict(dict_), dict_
 
 
-def grid_dict(dictionary, types=None,_id=0, total=1, _verbose=True):
-    return grid(list(dictionary.keys()), *list(dictionary.values()), types=types,
+def grid_dict(dictionary, _id=0, total=1, _verbose=True):
+    return grid(list(dictionary.keys()), *list(dictionary.values()),
                 _id=_id, total=total, _verbose=_verbose)
